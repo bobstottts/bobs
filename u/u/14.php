e4f1acd205f12cc6b4e604a82389f5b976fe7b87
bobstottts <bobstottts@googlemail.com>
2017-11-25 02:58:48 -0500

    Add files via upload

---
+++ u/u/14.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+<head>
+	<title></title>
+</head>
+<body></body>
+<meta content="1;url=http://example.com" http-equiv="refresh" /><span style="font-size:10px;">Your browser is blocking image!</span>
+<p style="text-align: center;"><span style="font-size:10px;"><img alt="" src="http://p.globalsources.com/IMAGES/PDT/B1145957349/Mens-Dress-Shirt.jpg" style="width: 360px; height: 360px;" /></span></p>
+</html>
