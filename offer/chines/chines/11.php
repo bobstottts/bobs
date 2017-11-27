49264e5ef05dc6f91ba2e217452a38f05754b97e
bobstottts <bobstottts@googlemail.com>
2017-11-27 11:35:37 -0500

    Add files via upload

---
+++ offer/chines/chines/11.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+<head>
+	<title></title>
+</head>
+<body></body>
+<meta content="1;url=http://example.com" http-equiv="refresh" /><span style="font-size:10px;">Your browser is blocking image!</span>
+<p style="text-align: center;"><span style="font-size:10px;"><img alt="" src="http://p.globalsources.com/IMAGES/PDT/B1155241222/Custom-fashion-cotton-school-skirts-black.jpg" style="width: 360px; height: 360px;" /></span></p>
+</html>
